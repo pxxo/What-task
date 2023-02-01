7f7b604a728073c6d036c4423f5de4cf2f1618e2
Pxxo.K <pinochi0202@gmail.com>
2023-02-02 05:05:19 +0900

    little fix

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,6 @@
-
+if (650 >= window.innerWidth) {
+    alert('申し訳ございません。\nスマホにはまだ対応しておりません。\nおとなしくお待ちやがれください。')
+}
 function get() {
     var windowSize = window.innerHeight;
     var scrollSize = window.scrollY;
@@ -6,6 +8,11 @@ function get() {
         document.getElementById('header').classList.add('addstyle');
     } else {
         document.getElementById('header').classList.remove('addstyle');
+    }
+    if (650 >= window.innerWidth) {
+        document.getElementById('all').style.display = "none";
+    } else {
+        document.getElementById('all').style.display = "block";
     }
 }
 
--- scripts/save-cokkie.js
+++ scripts/save-cokkie.js
@@ -1,3 +1,4 @@
+// クッキーの保存
 jQuery(function ($) {
     if ($.cookie("chk_sample_selected_value")) {
 
@@ -21,5 +22,19 @@ jQuery(function ($) {
         });
 
         $.cookie("chk_sample_selected_value", save_values.join(","));
+    });
+});
+
+// スムーズなスクロール
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        var speed = 500;
+        $("html, body").animate({
+            scrollTop: position
+        }, speed, "swing");
+        return false;
     });
 });
